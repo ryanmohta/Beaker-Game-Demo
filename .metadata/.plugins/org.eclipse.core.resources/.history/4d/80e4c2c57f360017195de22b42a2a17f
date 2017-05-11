9e5e3cdd8f669cdaabcf5f391f189da9cff5fa03
Pratha B <prathabodas@gmail.com>
2017-05-11 12:27:11 -0700

    Import fixes

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4d/80e4c2c57f360017195de22b42a2a17f
@@ -0,0 +1,16 @@
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
+
+
+public abstract class Actor extends ImageView {
+
+	public Actor(Element elem) {
+		super(elem);
+	}
+
+	//Overridden by each class
+	public void act(long now) {
+		
+	};
+	
+}
--- P5_Group_2/src/Actor.java
+++ P5_Group_2/src/Actor.java
@@ -1,13 +1,7 @@
-import javax.swing.text.Element;
-import javax.swing.text.html.ImageView;
-
+import javafx.scene.image.ImageView;
 
 public abstract class Actor extends ImageView {
-
-	public Actor(Element elem) {
-		super(elem);
-	}
-
+	
 	//Overridden by each class
 	public void act(long now) {
 		
