d92c339faa9e51341b53ae93a66c7180cac376e0
ryanmohta <ryanmohta@gmail.com>
2017-05-12 17:09:53 -0700

    Added LiquidDrop JavaDoc

---
+++ P5_Group_2/src/game/LiquidDrop.java
@@ -0,0 +1,73 @@
+package game;
+
+import gameEngine.*;
+import javafx.scene.image.Image;
+
+/**
+ * A {@code LiquidDrop} is an {@code Actor} that is able
+ * to be released upon the click of the dropper. Once released,
+ * the drop will fall straight down and could possibly fall
+ * into the moving beaker depending on the accuracy of the click.
+ * If the timing of the click is not accurate, the drop will
+ * fall on the ground.
+ *
+ * @author Pratha Bodas and Oleon Hariharan
+ */
+public class LiquidDrop extends Actor {
+
+    private double speed;
+
+    /**
+     * Creates a new {@code LiquidDrop} object with starting
+     * coordinates at {@code (0, 0)} and starting speed of 10.
+     */
+    public LiquidDrop(){
+        this(10);
+    }
+
+    /**
+     * Creates a new {@code LiquidDrop} object with starting
+     * coordinates at {@code (0, 0)} and specified starting
+     * speed.
+     * @param speed the starting speed
+     */
+    public LiquidDrop(double speed) {
+        this(0, 0, speed);
+    }
+
+    /**
+     * Creates a new {@code LiquidDrop} object with specified
+     * starting coordinates and speed.
+     * @param startX the starting x-coordinate
+     * @param startY the starting y-coordinate
+     * @param speed the starting speed
+     */
+    public LiquidDrop(double startX, double startY, double speed) {
+        super(startX, startY, new Image("file:///images/beaker.png"));
+        this.speed = speed;
+    }
+
+    /**
+     * Allows for the liquid drop to fall, resetting the {@code x} and
+     * {@code y} positions every millisecond.
+     */
+    public void fall() {
+        //method stub
+    }
+
+    /**
+     * Returns if this liquid drop is touching the floor
+     * @return if this liquid drop is touching the floor
+     */
+    public boolean isTouchingFloor() {
+        return true; //will fix later
+    }
+
+    /**
+     * Called every frame of the program lifecycle.
+     */
+    @Override
+    public void act() {
+
+    }
+}
