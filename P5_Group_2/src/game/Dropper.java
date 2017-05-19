c7a14b6ceeef1948af11ae72e0ec5560190c07d4
Pratha B <prathabodas@gmail.com>
2017-05-18 21:48:38 -0700

    Addition of the Dropper Class

---
+++ P5_Group_2/src/game/Dropper.java
@@ -0,0 +1,64 @@
+package game;
+
+import java.awt.event.MouseListener;
+import gameEngine.Actor;
+
+public class Dropper extends Actor implements MouseListener {
+	private double XPos, YPos;
+
+	public void clicked() {
+		
+	}
+
+	public void setXPos(double newpos) {
+		
+	}
+
+	public double getXPos() {
+		return XPos;
+	}
+
+	public void setYPos(double newpos) {
+		
+	}
+
+	public double getYPos() {
+		return YPos;
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
