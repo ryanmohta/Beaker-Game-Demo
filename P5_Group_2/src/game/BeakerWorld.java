e2311f9beae67dbac754becd2ac2a6b69b248c16
ryanmohta <ryanmohta@gmail.com>
2017-05-18 23:59:51 -0700

    Added things back

--- P5_Group_2/src/game/BeakerWorld.java
+++ P5_Group_2/src/game/BeakerWorld.java
@@ -1,9 +1,55 @@
 package game;
-import gameEngine.World;
 
+import gameEngine.Actor;
+import gameEngine.World;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Documentation coming soon
+ *
+ * @author Ryan Mohta
+ */
 public class BeakerWorld extends World {
+
+	private int secondCount = 0;
+	private double actCount = 0;
+	
+	public BeakerWorld() {
+		sceneProperty().addListener(new ChangeListener<Scene>() {
+			@Override
+			public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+				if(newValue != null) {
+					newValue.setOnKeyPressed(new EventHandler<KeyEvent>() {
+						@Override
+						public void handle(KeyEvent event) {
+							if(getOnKeyPressed() != null) {
+								getOnKeyPressed().handle(event);
+							}
+
+							for(Actor actor : getObjects(Actor.class)) {
+								if(actor.getOnKeyPressed() != null) {
+									actor.getOnKeyPressed().handle(event);
+								}
+							}
+						}
+					});
+				}
+			}
+		});
+	}
+
 	@Override
 	public void act(long now) {
-
+		int currentActCount = (int)(now / 10E8);
+		if(currentActCount >= actCount + 1) {
+			actCount = currentActCount;
+			System.out.println(++secondCount + " "); //tests if it works correctly
+		}
+		
 	}
+
 }
