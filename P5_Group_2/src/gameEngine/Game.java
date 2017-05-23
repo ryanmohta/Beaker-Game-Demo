9d8e9718ba3b77f363aa12779104c9bba3f2ee17
ryanmohta <ryanmohta@gmail.com>
2017-05-23 14:51:44 -0700

    Improved the MenuBar
    Added a Quit button and a divider between that and the new games

--- P5_Group_2/src/gameEngine/Game.java
+++ P5_Group_2/src/gameEngine/Game.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -18,8 +19,6 @@ public class Game extends Application implements ModelListener {
 	
 	private boolean mode; // true = custom, false = express
 	private int numFailed; //number of drops that hit the floor
-	private MenuItem express;
-	private MenuItem custom;
 	
 	private Text scoreboard; //the scoreboard (obviously)
 
@@ -31,14 +30,16 @@ public class Game extends Application implements ModelListener {
 		BeakerWorld world = new BeakerWorld(model);
 		BorderPane bP = new BorderPane(world);
 		
+		MenuBar menubar = new MenuBar();
+			Menu file = new Menu("File");
+				MenuItem newExpress = new MenuItem("New Express Game");
+				MenuItem newCustom = new MenuItem("New Custom Game...");
+				MenuItem quit = new MenuItem("Quit");
+				file.getItems().addAll(newExpress, newCustom, new SeparatorMenuItem(), quit);
+		menubar.getMenus().addAll(file);
+		
 		scoreboard = new Text("Score: 0");
 		scoreboard.setFont(new Font(20));
-		MenuBar menubar = new MenuBar();
-		Menu file = new Menu("File");
-		express = new MenuItem("Express Mode");
-		custom = new MenuItem("Custom Mode");
-		file.getItems().addAll(express, custom);
-		menubar.getMenus().add(file);
 		HBox hB = new HBox(20, scoreboard);
 		hB.setPadding(new Insets(20, 50, 20, 700));
 		bP.setBottom(hB);
