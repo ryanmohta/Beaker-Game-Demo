10253a0a0204faa29699d828b80bde85d71434d2
ryanmohta <ryanmohta@gmail.com>
2017-05-24 17:06:37 -0700

    Added documentation to the ModelListener interface

--- P5_Group_2/src/game/ModelListener.java
+++ P5_Group_2/src/game/ModelListener.java
@@ -1,8 +1,20 @@
 package game;
 
+/**
+ * An interface for listeners of the {@link Model} class.
+ *
+ * @author Ryan Mohta
+ */
 public interface ModelListener {
-	
+
+	/**
+	 * Performs a certain action when the score is changed.
+	 */
 	void scoreChanged(int newValue);
-	
+
+	/**
+	 * Performs a certain action when the state of the game
+	 * being finished is changed.
+	 */
 	void gameFinishedChanged(boolean newValue);
 }
